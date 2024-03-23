d511860287b086bcfd65081485ba96cd0b0cd625
Diminikhui <129111899+Diminikhui@users.noreply.github.com>
2024-03-24 00:01:14 +0300

    commit 22

--- js/renderProducts.js
+++ js/renderProducts.js
@@ -40,12 +40,12 @@ function renderProducts(productsArray) {
 									</div>
 								</div>
 
- <p class="hidden-description">
+ <button class="hidden-description">
  <img class="product-img" src="img/roll/${item.imgSrc}" alt="">
  
  ${item.description}
  
- </p>
+ </button>
 
 
 								<button data-cart type="button" class="btn btn-block btn-outline-warning">
