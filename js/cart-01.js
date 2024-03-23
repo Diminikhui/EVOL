17f8451da69d8eebac3a83aa33ff87d3ace1c3a8
Diminikhui <129111899+Diminikhui@users.noreply.github.com>
2024-03-23 19:28:44 +0300

    commit 19

--- js/cart-01.js
+++ js/cart-01.js
@@ -27,7 +27,7 @@ window.addEventListener('click', function (event) {
 									</div>
 									<div class="cart-item__desc">
 										<div class="cart-item__title">${productInfo.title}</div>
-										<div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>
+										<!--  <div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}</div>-->
 
 										<!-- cart-item__details -->
 										<div class="cart-item__details">
